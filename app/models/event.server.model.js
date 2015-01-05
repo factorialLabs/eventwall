9b6a2d544b8742fce9bcede0533ab5fe32a44ba0
Yu Chen Hou <houyuchen66@gmail.com>
2015-01-04 23:17:04 -0500

    Updated the Event Model

--- app/models/event.server.model.js
+++ app/models/event.server.model.js
@@ -16,6 +16,39 @@ var EventSchema = new Schema({
 		required: 'Please fill Event name',
 		trim: true
 	},
+    category: {
+		type: String,
+        enum: ['Uncategorized', 'Club Event'],
+		default: 'Uncategorized',
+		required: 'Please enter a category',
+		trim: true
+	},
+    datetime_start: {
+		type: Date,
+        default: Date.now,
+		required: 'Please enter a date and time'
+	},
+    datetime_end: {
+		type: Date,
+        default: Date.now,
+		required: 'Please enter a date and time'
+	},
+    location: {
+		type: String,
+        default: '',
+		required: 'Please enter a location',
+        trim: true
+	},
+    description: {
+		type: String,
+        default: '',
+        trim: true
+	},
+    thumbnail: {
+		type: String,
+        default: '',
+        trim: true
+	},
 	created: {
 		type: Date,
 		default: Date.now
