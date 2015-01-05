787edcdbc041660053107a92eae5cce0ab3c98b6
Yu Chen Hou <houyuchen66@gmail.com>
2015-01-05 01:09:32 -0500

    Working event Grid

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,9 +1,16 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$resource',
+	function($scope, Authentication,$resource) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+        $scope.events = [{name:'None :('}];
+
+        // Find a list of Events
+        $scope.Events = $resource('events/:eventId', { eventId: '@_id'
+    });
+        $scope.events = $scope.Events.query();
+        console.log($scope.events);
 	}
 ]);
