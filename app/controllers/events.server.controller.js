b0ddbd788d34cf3e1046ba904e29bc32e0b6ae8a
Yu Chen Hou <houyuchen66@gmail.com>
2015-01-06 13:46:43 -0500

    Added date filtering

--- app/controllers/events.server.controller.js
+++ app/controllers/events.server.controller.js
@@ -73,7 +73,7 @@ exports.delete = function(req, res) {
  * List of Events
  */
 exports.list = function(req, res) { 
-	Event.find().sort('-created').populate('user', 'displayName').exec(function(err, events) {
+	Event.find().where('datetime_end').gt(new Date()).sort('-datetime_start').populate('user', 'displayName').exec(function(err, events) {
 		if (err) {
 			return res.status(400).send({
 				message: errorHandler.getErrorMessage(err)
