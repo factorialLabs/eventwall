f70ee98188f9743a04acc7502ec2a427a0329459
Deon Hua <deonhbci@gmail.com>
2015-02-28 02:29:04 -0500

    Added oauth.js placeholder
    Added an oauth.js placeholder for TravisCI builds.

---
+++ config/env/oauth.js
@@ -0,0 +1,22 @@
+module.exports = {
+    fbId: '',
+    fbSecret: '',
+    twitterId: '',
+    twitterSecret: '',
+    googleId: '',
+    googleSecret: '',    
+    linkedInId: '',
+    linkedInSecret: '',
+    gitId: '',
+    gitSecret: '',
+    fbDevId: '',
+    fbDevSecret: '',
+    twitterDevId: '',
+    twitterDevSecret: '',
+    googleDevId: '',
+    googleDevSecret: '',
+    linkedInDevId: '',
+    linkedInDevSecret: '',
+    gitDevId: '',
+    gitDevSecret: ''
+};
