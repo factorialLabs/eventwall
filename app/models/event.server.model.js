5afc5eadbdb78ee4f71d4e8f509b6c48f1c92097
Yu Chen Hou <houyuchen66@gmail.com>
2015-03-03 11:43:30 -0500

    valdate dates

--- app/models/event.server.model.js
+++ app/models/event.server.model.js
@@ -65,4 +65,20 @@ var EventSchema = new Schema({
     }
 });
 
+/**
+ * A validation check so event datetimes makes sense
+ */
+EventSchema.pre("validate", function(next) {
+    if(this.datetime_end<this.datetime_start ||
+      this.datetime_start<new Date()){
+            console.log("Invalid");
+            this.invalidate("datetime_start", "Start and End time must be valid");
+            //next(new Error("Start and End time must be valid"));
+        } else {
+            console.log("valid");
+            next();
+        }
+    }
+);
+
 mongoose.model('Event', EventSchema);
