0f08190ef0833bb8143d8c3f100eec189c399f12
Deon Hua <deonhbci@gmail.com>
2015-03-03 10:54:45 -0500

    Cleaned up Oauth System

---
+++ config/env/oauthTest.js
@@ -0,0 +1,13 @@
+// Purely for TravisCI builds, don't place production/development keys here.
+module.exports = {
+    fbTestId: 'asdasd',
+    fbTestSecret: 'sadsd',
+    twitterTestId: 'asdasd',
+    twitterTestSecret: 'asdasd',
+    googleTestId: 'asasdasd',
+    googleTestSecret: 'asdasdasd',
+    linkedInTestId: 'asdasdasd',
+    linkedInTestSecret: 'asdasda',
+    gitTestId: 'asdasd',
+    gitTestSecret: 'asdads'
+};
